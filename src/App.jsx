c5f1bee601eed4562fae227bc519ee5aedff18a2
Omar Farouk Dahmani <omar.farouk.dahmani.contact@gmail.com>
2025-06-08 23:00:43 +0200

    adding new feature (dark mode )

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from '../src/pages/Home'
+// import DarkMode from './components/DarkMode/DarkMode'
 import './App.css'
 
 function App() {
   return (
     <>
+      {/* <DarkMode/> */}
       <Home/>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
- import logo from '../assets/pizone_logo.png'
+import logo from '../assets/pizone_logo.png'
+import DarkMode from './DarkMode/DarkMode' 
+
  export default function Navbar(){
     return (
         <div className="Navbar">
@@ -17,6 +19,7 @@
                 <button id="log_in">Log in</button>
                 <button id="sign_in">Sign in</button>
             </div>
+            <DarkMode/>
         </div>
     )
  }
