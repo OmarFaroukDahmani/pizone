f68b6467cb3c40172a4789230718ab6884a1f0b9
Omar Farouk Dahmani <omar.farouk.dahmani.contact@gmail.com>
2025-06-08 10:55:56 +0200

    Upload the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+ import logo from '../assets/pizone_logo.png'
+ export default function Navbar(){
+    return (
+        <div className="Navbar">
+            <a href="">
+            <img src={logo} className='Navbar_logo' alt="pizone_logo" />
+            </a>
+            <nav className="Navbar_links">
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Our offers</a></li>
+                    <li><a href="">Partners</a></li>
+                    <li><a href="">Courses</a></li>
+                </ul>
+            </nav>
+            <div className="Navbar_logs">
+                <button id="log_in">Log in</button>
+                <button id="sign_in">Sign in</button>
+            </div>
+        </div>
+    )
+ }
