acca104062516d434067e203395aa0575676ee92
Omar Farouk Dahmani <omar.farouk.dahmani.contact@gmail.com>
2025-06-12 16:58:33 +0200

    editing the code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2023 Pi Zone. All rights reserved.</p>
+                <ul className="footer-links">
+                    <li><a href="">About</a></li>
+                    <li><a href="">Our offers</a></li>
+                    <li><a href="">Partners</a></li>
+                    <li><a href="">Courses</a></li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
